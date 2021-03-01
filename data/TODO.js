62962cb86ba5f1715b9680ca61801f9e94d36997
phsequeira <phsequeira@yahoo.com>
2021-03-01 13:12:41 -0800

    fixed data

---
+++ data/TODO.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'take out trash',
+    completed: false
+  },
+  {
+    todo: 'sweep the kitchen',
+    completed: false
+  },
+  {
+    todo: 'make the bed',
+    completed: true
+  }
+];
